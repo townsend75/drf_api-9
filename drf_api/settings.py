7af610e07806c979585a6c5925e494aabcfc1fe7
townsend75 Townsend <dtownsend@gmx.net>
2023-11-16 20:26:12 +0000

    add Profile model and automatically create profile for new users

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+if os.path.exists('env.py'):
+    import env
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +35,7 @@ SECRET_KEY = 'django-insecure-a=)y+2$p#d3_-(2j9@h6_omou)#gk615ytulf4flr(4^rkg_s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-townsend75-drfapi9-ttdazophfeu.ws-eu106.gitpod.io']
 
 
 # Application definition
@@ -36,7 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+
+    'profiles'
 ]
 
 MIDDLEWARE = [
