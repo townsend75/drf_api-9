842036cc6bb2ed4f6da25c56be67d1e9b1cbdea2
townsend75 Townsend <dtownsend@gmx.net>
2023-11-18 18:10:29 +0000

    add integrity error to like serializer

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     
+    
 ]
--- likes/serializers.py
+++ likes/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Likes
+from .models import Like
 
 
 class LikesSerializer(serializers.ModelSerializer):
